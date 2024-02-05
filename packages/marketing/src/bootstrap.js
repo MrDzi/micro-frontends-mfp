6506a4b0bfd10da15d7e24d5f03ac50649a3c652
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2024-02-05 17:36:58 +0100

    use browser history in the marketing app for local development

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -27,10 +28,10 @@ const mount = (el, { onNavigate }) => {
 };
 
 if (process.env.NODE_ENV === "development") {
-  const el = document.querySelector("#_container-dev-root");
+  const el = document.querySelector("#_marketing-dev-root");
 
   if (el) {
-    mount(el, {});
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
